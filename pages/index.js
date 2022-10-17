4ab4ade43d8a02fa3413fce0c76ca691e5ce0b37
Alapottra <alapottratrojon@gmail.com>
2022-10-17 17:14:13 +0600

    3rd section done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/home.module.css'
+import { HiOutlineMinus } from 'react-icons/hi';
+import { GiCheckMark } from 'react-icons/gi';
+
 export default function Home() {
 
 
@@ -26,7 +29,7 @@ export default function Home() {
 
       {/*******  2ND SECTION *************/}
 
-      <div className='py-32 bg-white container mx-auto  font-type px-16'>
+      <div className='py-20 bg-white container mx-auto  font-type px-16'>
 
         <h2 className='font-extra text-center text-5xl' >The Art of Making Memories Tangible</h2>
         <div className='border-b-2 mt-7 w-[50%] mx-auto border-main' ></div>
@@ -69,44 +72,245 @@ export default function Home() {
 
 
 
-      <div className='py-32 bg-sky-50/50' >
+      <div className='py-20 bg-sky-50/50' >
         <div className='container mx-auto' >
           <h2 className='font-display text-center text-3xl font-semibold' >OUR PACKAGES</h2>
 
-          HiOutlineMinus
+
           {/***************** PACKAGES *****************/}
 
-          <div className='grid grid-cols-3 mt-20' >
+          <div className='grid grid-cols-3 mt-20 gap-16' >
 
+            {/***************** PACKAGE 1 *****************/}
 
-            <div className='flex flex-col bg-white p-12' >
+            <div className='flex flex-col bg-white p-12 shadow-sm' >
 
               <div className='text-center font-display font-semibold' >
-                <div className='flex justify-center' >
-                  <div className='border-b-2' ></div>
-                  <h2 className='text-2xl ' >THE BASIC</h2>
-                  <div className='border-b-2' ></div>
+
+                <div className='flex justify-center items-center text-3xl' >
+                  <HiOutlineMinus className='text-main' />
+                  <h2 className='text-2xl mx-4 ' >THE BASIC</h2>
+                  <HiOutlineMinus className='text-main' />
                 </div>
+
+                <h2 className='text-5xl mt-5' > <span className='text-main' >13,000</span> BDT</h2>
+
+
+              </div>
+
+              <div className='mt-8 font-type flex flex-col gap-6'>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>1 Photographer</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-white text-xl' />
+                  <h2 className='px-4'>1 Cinematographer</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Duration 1 Day (8.00 am - 10.30 pm)</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Print : 50 4r high quality matte print</h2>
+                </div>
+
+              
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-white' />
+                  <h2 className='px-4'>Premium Photo Album with 12L Frame</h2>
+                </div>
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-white' />
+                  <h2 className='px-4'>2-5 minutes Video trailer</h2>
+                </div>
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-white' />
+                  <h2 className='px-4'>15-30 minutes full video </h2>
+                </div>
+
+
+              </div>
+
+              <div className='mt-9 text-center font-display text-xl '>
+
+                <button className='px-10 py-3 border-2 border-main rounded-md hover:bg-main hover:text-white transition-all' >Get Started</button>
+
               </div>
 
 
-            </div>
 
-            <div>
 
             </div>
+            {/***************** PACKAGE 2 *****************/}
 
-            <div>
+            <div className='flex flex-col bg-white p-12 shadow-sm' >
+
+              <div className='text-center font-display font-semibold' >
+
+                <div className='flex justify-center items-center text-3xl' >
+                  <HiOutlineMinus className='text-main' />
+                  <h2 className='text-2xl mx-4 ' >THE MOMENTO</h2>
+                  <HiOutlineMinus className='text-main' />
+                </div>
+
+                <h2 className='text-5xl mt-5' > <span className='text-main' >36,500</span> BDT</h2>
+
+
+              </div>
+
+              <div className='mt-8 font-type flex flex-col gap-6'>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Anik & 1 Photographer</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-main' />
+                  <h2 className='px-4'>1 Cinematographer</h2>
+                </div>
+
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Duration 2 Day (8.00 am - 10.30 pm)</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Print : 50 copies 4r size, 20 copies 5L size </h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-white text-xl' />
+                  <h2 className='px-4'>Premium Photo Album with 12L Frame</h2>
+                </div>
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-main' />
+                  <h2 className='px-4'>2-5 minutes Video trailer</h2>
+                </div>
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-main' />
+                  <h2 className='px-4'>15-30 minutes full video </h2>
+                </div>
+              
+
+
+              </div>
+
+              <div className='mt-9 text-center font-display text-xl '>
+
+                <button className='px-10 py-3 border-2 border-main rounded-md hover:bg-main hover:text-white transition-all' >Get Started</button>
+
+              </div>
+
+
+
+
+            </div>
+
+            {/***************** PACKAGE 3 *****************/}
+
+            <div className='flex flex-col bg-white p-12 shadow-sm' >
+
+              <div className='text-center font-display font-semibold' >
+
+                <div className='flex justify-center items-center text-3xl' >
+                  <HiOutlineMinus className='text-main' />
+                  <h2 className='text-2xl mx-4 ' >THE SIGNATURE</h2>
+                  <HiOutlineMinus className='text-main' />
+                </div>
+
+                <h2 className='text-5xl mt-5' > <span className='text-main' >60,000</span> BDT</h2>
+
+
+              </div>
+
+              <div className='mt-8 font-type flex flex-col gap-6'>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Antar, Anik & 1 Photographer</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-main' />
+                  <h2 className='px-4'>2 Cinematographer</h2>
+                </div>
+
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Duration 2 Day (8.00 am - 10.30 pm)</h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Print : 50 copies 4r size, 20 copies 5L size </h2>
+                </div>
+
+                <div className='flex items-center' >
+                  <GiCheckMark className='text-main text-xl' />
+                  <h2 className='px-4'>Premium Photo Album with 12L Frame</h2>
+                </div>
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-main' />
+                  <h2 className='px-4'>2-5 minutes Video trailer</h2>
+                </div>
+                <div className='flex items-center' >
+                  <GiCheckMark className=' text-xl text-main' />
+                  <h2 className='px-4'>15-30 minutes full video </h2>
+                </div>
+              
+
+
+              </div>
+
+              <div className='mt-9 text-center font-display text-xl '>
+
+                <button className='px-10 py-3 border-2 border-main rounded-md hover:bg-main hover:text-white transition-all' >Get Started</button>
+
+              </div>
+
+
+
 
             </div>
 
 
+
+
+
+
+          </div>
+
+          <div className='text-center mt-14'>
+                
+                    <button className='px-10 py-2 bg-gray-300/75 font-display text-xl rounded-md'>See more</button>
+                   
           </div>
 
 
 
         </div>
 
+
+      </div>
+
+
+
+
+
+
+      {/************* 4TH SECTION *************/}
+
+      <div className='py-32 bg-white'>
 
       </div>
 
