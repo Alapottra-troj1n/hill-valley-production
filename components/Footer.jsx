37c6c394a3fff020ba63bf874efb5368a9aa89e7
Alapottra <alapottratrojon@gmail.com>
2022-10-20 00:26:26 +0600

    designed dynamic post page and fixed footer links misplacement

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -34,8 +34,8 @@ const Footer = () => {
                 <h2 className='text-2xl font-semibold font-display' >Stay with us</h2>
                 <div className='flex items-center gap-4' >
                     <a href='https://www.facebook.com/hillvalleyproduction' target='_blank' rel="noreferrer" ><AiFillFacebook className='text-3xl cursor-pointer hover:text-main transition-all' /></a>
-                    <a href='https://www.instagram.com/productionhillvalley/' target='_blank' rel="noreferrer" ><AiFillYoutube className='text-4xl cursor-pointer hover:text-main transition-all' /></a>
-                    <a href='https://www.youtube.com/c/HillValleyProduction' target='_blank' rel="noreferrer" > <AiFillInstagram className='text-3xl cursor-pointer hover:text-main transition-all' /></a>
+                    <a href='https://www.youtube.com/c/HillValleyProduction' target='_blank' rel="noreferrer" ><AiFillYoutube className='text-4xl cursor-pointer hover:text-main transition-all' /></a>
+                    <a href='https://www.instagram.com/productionhillvalley/' target='_blank' rel="noreferrer" > <AiFillInstagram className='text-3xl cursor-pointer hover:text-main transition-all' /></a>
                 </div>
             </div>
 
--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -16,18 +16,70 @@ const Post = () => {
             </div>
 
             <div className='py-24 container mx-auto text-slate-500 font-normal font-type  lg:px-40  px-4'>
-              
-                
-             
+
+
+
 
                 {/*************** PHOTOS START FROM HERE ***************/}
                 <h2 className='leading-6 text-xs lg:text-sm lg:leading-7'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum vitae rerum ratione delectus possimus facere consequuntur ea necessitatibus provident itaque. Corrupti, voluptas debitis. Aut, recusandae modi ducimus possimus eaque voluptate aliquid quas quos laborum minus iusto temporibus ab quo vero officia asperiores necessitatibus doloribus praesentium? Molestiae totam ea, sequi veniam quibusdam unde nesciunt. Culpa perspiciatis, ipsa,  sunt unde nostrum cupiditate possimus</h2>
 
-                <div className="py-20 flex flex-col items-center lg:px-0" >
+                <div className="py-20 grid grid-cols-1 lg:px-0 gap-10" >
+
+
 
                     <div className="relative lg:h-[700px] h-[400px] w-full " >
-                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={''} />
+                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
                     </div>
+
+                    <div className="relative lg:h-[700px] h-[400px] w-full " >
+                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                    </div>
+
+                    {/***************** 2 COLUMNS *****************/}
+
+                    <div className="grid grid-cols-2 gap-10">
+
+                        <div className="relative lg:h-[700px] h-[400px] w-full " >
+                            <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                        </div>
+                        <div className="relative lg:h-[700px] h-[400px] w-full " >
+                            <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                        </div>
+
+                    </div>
+
+                    <div className="relative lg:h-[700px] h-[400px] w-full " >
+                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                    </div>
+
+                    <div className="relative lg:h-[700px] h-[400px] w-full " >
+                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                    </div>
+
+
+                    {/***************** 2 COLUMNS *****************/}
+
+                    <div className="grid grid-cols-2 gap-10">
+
+                        <div className="relative lg:h-[700px] h-[400px] w-full " >
+                            <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                        </div>
+                        <div className="relative lg:h-[700px] h-[400px] w-full " >
+                            <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                        </div>
+
+                    </div>
+
+
+
+                    <div className="relative lg:h-[700px] h-[400px] w-full " >
+                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                    </div>
+
+                    <div className="relative lg:h-[700px] h-[400px] w-full " >
+                        <Image src='https://i.ibb.co/nCQbXL2/74387413-2447623832174963-8578973925654921216-n-1.jpg' layout='fill' objectFit='cover' objectPosition={'center'} />
+                    </div>
+
 
 
 
@@ -35,6 +87,16 @@ const Post = () => {
 
 
 
+
+
+            </div>
+
+            <div className='text-slate-500 pb-10' >
+                <div className='text-center' >
+                    <h2 className='font-display text-2xl' >Follow us on Instagram</h2>
+                    <a href={'https://www.instagram.com/productionhillvalley/'} target='_blank' rel="noreferrer" className='font-type text-md cursor-pointer hover:text-main transition-all' >@productionhillvalley</a>
+
+                </div>
 
             </div>
 
