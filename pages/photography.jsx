a47cc7bfb990091b1be71b7e3b486221f7d2b864
Alapottra <alapottratrojon@gmail.com>
2022-10-20 16:04:57 +0600

    added bg change on photography page

--- pages/photography.jsx
+++ pages/photography.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Fashion from '../components/Photography/Fashion';
 import Food from '../components/Photography/Food';
 import Wedding from '../components/Photography/Wedding';
@@ -8,10 +8,29 @@ const Photography = () => {
 
     const [currentPage, setCurrentPage] = useState('wedding');
 
+    const [bg, setBg] = useState('https://i.ibb.co/1GYBJ0L/dfg.jpg');
+
+
+    useEffect(() => {
+
+        if (currentPage === 'wedding') {
+            setBg('https://i.ibb.co/1GYBJ0L/dfg.jpg')
+        } else if (currentPage === 'food') {
+            setBg('https://i.ibb.co/Ntryhf7/food-bg.jpg')
+        }
+
+
+
+    }, [currentPage])
+
+    console.log(bg);
+
 
     return (
         <div>
-            <div className="flex justify-center items-center bg-[url('https://i.ibb.co/wz4P1fp/display-picture.jpg')] bg-no-repeat bg-cover bg-center bg-black lg:h-[80vh] h-[50vh] bg-[center_top_-44rem] bg-fixed" >
+            <div style={{
+                backgroundImage: `url(${bg})`,
+            }} className={`flex justify-center items-center bg-no-repeat bg-cover lg:h-[80vh] h-[50vh] ${currentPage === 'wedding' ? 'bg-[center_top_-44rem]' : 'bg-center'} bg-fixed`} >
 
 
 
@@ -21,14 +40,14 @@ const Photography = () => {
             <div className='py-32 flex justify-center' >
 
                 <div className='grid grid-cols-3 gap-36 font-display text-xl ' >
-                    <div className='hover:text-main'>
-                        <h2 onClick={()=> setCurrentPage('wedding')} className='cursor-pointer  transition-all' >WEDDING</h2>
+                    <div className='hover:text-main text-center'>
+                        <h2 onClick={() => setCurrentPage('wedding')} className={`cursor-pointer  transition-all border-b-2 ${currentPage === 'wedding' ? ' border-main' : 'border-white'}`} >WEDDING</h2>
                     </div>
-                    <div className='hover:text-main'>
-                        <h2 onClick={()=> setCurrentPage('fashion')} className='cursor-pointer transition-all' >FASHION</h2>
+                    <div className='hover:text-main text-center'>
+                        <h2 onClick={() => setCurrentPage('fashion')} className={`cursor-pointer transition-all border-b-2 ${currentPage === 'fashion' ? ' border-main' : 'border-white'} `} >FASHION</h2>
                     </div>
-                    <div className='hover:text-main' >
-                        <h2 onClick={()=> setCurrentPage('food')} className='cursor-pointer   transition-all' >FOOD</h2>
+                    <div className='hover:text-main text-center' >
+                        <h2 onClick={() => setCurrentPage('food')} className={`cursor-pointer   transition-all border-b-2 ${currentPage === 'food' ? ' border-main' : 'border-white'}`} >FOOD</h2>
                     </div>
 
                 </div>
@@ -36,13 +55,13 @@ const Photography = () => {
             </div>
 
 
-          <div className='py-16 flex justify-center' >
+            <div className='py-16 flex justify-center' >
 
-          {currentPage === 'wedding' && <Wedding/> }
-            {currentPage === 'fashion' && <Fashion/> }
-            {currentPage === 'food' && <Food/> }
+                {currentPage === 'wedding' && <Wedding />}
+                {currentPage === 'fashion' && <Fashion />}
+                {currentPage === 'food' && <Food />}
 
-          </div>
+            </div>
 
 
         </div>
