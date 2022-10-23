0d612c07a2b634ac9021a398af9d3af819c9bee7
Alapottra <alapottratrojon@gmail.com>
2022-10-24 00:36:28 +0600

    test6

--- components/Facebook.js
+++ components/Facebook.js
@@ -6,7 +6,7 @@ function Facebook() {
       <div id="fb-root"></div>
 
       <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script strategy="lazyOnload">
+      <Script id='facebookscript' strategy="lazyOnload">
         {`
             var chatbox = document.getElementById('fb-customer-chat');
             chatbox.setAttribute("page_id", "100044432706390");
